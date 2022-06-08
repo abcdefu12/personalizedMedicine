81e11a13d5dee6386bc5de619d3b7a84acdc385a
Gim <gbks081@gmail.com>
2022-06-09 01:14:07 +0900

    feat: logistic regression without class balancing(6.6)

--- main.py
+++ main.py
@@ -1417,11 +1417,76 @@ get_impfeature_names(indices[0], test_df['TEXT'].iloc[test_point_index], test_df
 ##############
 # 6.6 Logistic Regression without class balancing
 print('< Logistic Regression without class balancing >')
+
 # 6.6.1 Hyperparameter tuning
+alpha = [10 ** x for x in range(-6, 1)]
+cv_log_error_array = []
+for i in alpha:
+    print("for alpha =", i)
+    clf = SGDClassifier(alpha=i, penalty='l2', loss='log', random_state=42)
+    clf.fit(train_x_onehotCoding, train_y)
+    sig_clf = CalibratedClassifierCV(clf, method="sigmoid")
+    sig_clf.fit(train_x_onehotCoding, train_y)
+    sig_clf_probs = sig_clf.predict_proba(cv_x_onehotCoding)
+    cv_log_error_array.append(log_loss(cv_y, sig_clf_probs, labels=clf.classes_, eps=1e-15))
+    print("Log Loss :", log_loss(cv_y, sig_clf_probs))
+
+fig, ax = plt.subplots()
+ax.plot(alpha, cv_log_error_array, c='g')
+for i, txt in enumerate(np.round(cv_log_error_array, 3)):
+    ax.annotate((alpha[i], str(txt)), (alpha[i], cv_log_error_array[i]))
+plt.grid()
+plt.title("Cross Validation Error for each alpha")
+plt.xlabel("Alpha i's")
+plt.ylabel("Error measure")
+plt.show()
+
+best_alpha = np.argmin(cv_log_error_array)
+clf = SGDClassifier(alpha=alpha[best_alpha], penalty='l2', loss='log', random_state=42)
+clf.fit(train_x_onehotCoding, train_y)
+sig_clf = CalibratedClassifierCV(clf, method="sigmoid")
+sig_clf.fit(train_x_onehotCoding, train_y)
+
+predict_y = sig_clf.predict_proba(train_x_onehotCoding)
+print('For values of best alpha = ', alpha[best_alpha], "The train log loss is:",
+      log_loss(y_train, predict_y, labels=clf.classes_, eps=1e-15))
+predict_y = sig_clf.predict_proba(cv_x_onehotCoding)
+print('For values of best alpha = ', alpha[best_alpha], "The cross validation log loss is:",
+      log_loss(y_cv, predict_y, labels=clf.classes_, eps=1e-15))
+predict_y = sig_clf.predict_proba(test_x_onehotCoding)
+print('For values of best alpha = ', alpha[best_alpha], "The test log loss is:",
+      log_loss(y_test, predict_y, labels=clf.classes_, eps=1e-15))
+
 # 6.6.2 Testing the model with the best hyperparameter
+clf = SGDClassifier(alpha=alpha[best_alpha], penalty='l2', loss='log', random_state=42)
+predict_and_plot_confusion_matrix(train_x_onehotCoding, train_y, cv_x_onehotCoding, cv_y, clf)
+
 # 6.6.3 Feature importance
 # correctly classified point
+clf = SGDClassifier(alpha=alpha[best_alpha], penalty='l2', loss='log', random_state=42)
+clf.fit(train_x_onehotCoding, train_y)
+test_point_index = 1
+no_feature = 500
+predicted_cls = sig_clf.predict(test_x_onehotCoding[test_point_index])
+print("Predicted Class :", predicted_cls[0])
+print("Predicted Class Probabilities:", np.round(sig_clf.predict_proba(test_x_onehotCoding[test_point_index]), 4))
+print("Actual Class :", test_y[test_point_index])
+indices = np.argsort(abs(-clf.coef_))[predicted_cls - 1][:, :no_feature]
+print("-" * 50)
+get_impfeature_names(indices[0], test_df['TEXT'].iloc[test_point_index], test_df['Gene'].iloc[test_point_index],
+                     test_df['Variation'].iloc[test_point_index], no_feature)
+
 # Incorrectly classified point
+test_point_index = 100
+no_feature = 500
+predicted_cls = sig_clf.predict(test_x_onehotCoding[test_point_index])
+print("Predicted Class :", predicted_cls[0])
+print("Predicted Class Probabilities:", np.round(sig_clf.predict_proba(test_x_onehotCoding[test_point_index]), 4))
+print("Actual Class :", test_y[test_point_index])
+indices = np.argsort(abs(-clf.coef_))[predicted_cls - 1][:, :no_feature]
+print("-" * 50)
+get_impfeature_names(indices[0], test_df['TEXT'].iloc[test_point_index], test_df['Gene'].iloc[test_point_index],
+                     test_df['Variation'].iloc[test_point_index], no_feature)
 
 ##############
 # 6.7 Linear Support Vector Machines
